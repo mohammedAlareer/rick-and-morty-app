1e34d35aa88badab501911af1ec6e41714b4780a
Mohammed <you@example.com>
2025-12-10 05:36:33 +0200

    Modify the navbar and add features.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 
 export default function Navbar() {
      return (
-    <nav className='sticky top-0 z-50 shadow-lg bg-indigo-950/90 text-white backdrop-blur-xl border-b border-white/10'>
+    <nav className='sticky top-0 z-50 shadow-lg bg-slate-900 text-white backdrop-blur-xl border-b border-white/10'>
       <MaxWidthWrapper>
-        <div className='flex items-center py-4'>
-          <Link href='/' className='text-4xl font-bold '>
-            Rick & Morty
+        <div className='flex items-center py-5'>
+          <Link href='/' className='text-4xl font-bold animate-float '>
+            <span className='shadow-md shadow-green-400  rounded-full px-2 py-2'>Rick & Morty</span>
           </Link>
           </div>
         </MaxWidthWrapper>
